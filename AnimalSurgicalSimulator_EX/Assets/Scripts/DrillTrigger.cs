a44499380cc134a774780b2aefc711af1e36c53a
poiz1245 <wva960@gmail.com>
2024-07-16 14:25:35 +0900

    feat : Dotween 활용하여 드릴 회전 기능 추가

--- AnimalSurgicalSimulator_EX/Assets/Scripts/DrillTrigger.cs
+++ AnimalSurgicalSimulator_EX/Assets/Scripts/DrillTrigger.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections.LowLevel.Unsafe;
@@ -11,6 +12,7 @@ public class DrillTrigger : MonoBehaviour
 
     [SerializeField] InputActionReference triggerButton;
     [SerializeField] XRBaseController rightController;
+    [SerializeField] GameObject spinObject;
     //[SerializeField] XRController leftController;
     // Start is called before the first frame update
 
@@ -56,10 +58,12 @@ public class DrillTrigger : MonoBehaviour
     void TriggerButtonOn(InputAction.CallbackContext context)
     {
         buttonOn = true;
+        spinObject.transform.DOLocalRotate(new Vector3(0, 0, -1), 0.005f).SetEase(Ease.Linear).SetLoops(-1, LoopType.Incremental);
     }
     void TriggerButtonOff(InputAction.CallbackContext context)
     {
         buttonOn = false;
+        spinObject.transform.DOKill();
         OnVibration(0);
     }
     private void OnTriggerEnter(Collider other)
