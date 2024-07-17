98ec49c0eac93d71becd48b32c2c9b89955aa9f6
poiz1245 <wva960@gmail.com>
2024-07-17 17:45:26 +0900

    feat : indicator에 attach후 컨트롤러 z좌표에 따라서 앞뒤로 움직임 구현

--- AnimalSurgicalSimulator_EX/Assets/01.Scripts/AttachIndicator.cs
+++ AnimalSurgicalSimulator_EX/Assets/01.Scripts/AttachIndicator.cs
@@ -11,13 +11,8 @@ public class AttachIndicator : MonoBehaviour
     [SerializeField] GameObject handModel;
     [SerializeField] XRGrabInteractable grabInteractor;
     
-    ControllerZPositionTracker tracker;
     bool isAttach = false;
 
-    private void Start()
-    {
-        tracker = GetComponent<ControllerZPositionTracker>();
-    }
     private void Update()
     {
         float distance = Vector3.Distance(indicator.transform.position, gameObject.transform.position);
@@ -31,8 +26,7 @@ public class AttachIndicator : MonoBehaviour
         }
         else if (isAttach && grabInteractor.isSelected && distance <= 0.2f)
         {
-            print(tracker.zPositionDelta);
-            handModel.transform.position = new Vector3(indicatorAttach.transform.position.x, indicatorAttach.transform.position.y, indicatorAttach.transform.position.z + (tracker.zPositionDelta*0.05f));
+            handModel.transform.position = new Vector3(indicatorAttach.transform.position.x, indicatorAttach.transform.position.y, gameObject.transform.position.z);
             //handModel.transform.position = indicatorAttach.transform.position;
         }
         else if ((isAttach && distance > 0.2f) || !grabInteractor.isSelected)
