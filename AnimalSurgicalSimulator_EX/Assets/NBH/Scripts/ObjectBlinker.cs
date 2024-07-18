af4645e25f30f968c8a200b5df2cd8add2acd77a
Nabyeongho <127078321+nabh0203@users.noreply.github.com>
2024-07-18 16:04:50 +0900

    Blink , soket 제작 전 환경 구축

--- AnimalSurgicalSimulator_EX/Assets/NBH/Scripts/ObjectBlinker.cs
+++ AnimalSurgicalSimulator_EX/Assets/NBH/Scripts/ObjectBlinker.cs
@@ -8,17 +8,32 @@ public class ObjectBlinker : MonoBehaviour
     [SerializeField] XRBaseInteractor socketInteractor; // 소켓 인터렉터 참조
     [SerializeField] GameObject objectToBlink; // 깜빡이게 할 오브젝트
     [SerializeField] float blinkInterval = 0.5f; // 깜빡이는 간격
-    [SerializeField] XRGrabInteractable[] grabInteractor;
+    [SerializeField] List<XRGrabInteractable> grabInteractors; // XRGrabInteractable 리스트
 
     bool isBlinking = false;
     bool isObjectInSocket = false;
+    bool isGrabbed = false;
 
     void Start()
     {
         objectToBlink.SetActive(false);
+
+        // 각 grabInteractor에 대해 SelectEntered 이벤트를 등록
+        foreach (var grabInteractor in grabInteractors)
+        {
+            grabInteractor.selectEntered.AddListener(OnGrabbed);
+            grabInteractor.selectExited.AddListener(OnReleased);
+        }
     }
+
     void Update()
     {
+        if (!isGrabbed)
+        {
+            objectToBlink.SetActive(false);
+            return;
+        }
+
         // 소켓에 오브젝트가 있는지 확인
         isObjectInSocket = socketInteractor.hasSelection;
 
@@ -28,7 +43,7 @@ public class ObjectBlinker : MonoBehaviour
             objectToBlink.SetActive(true);
             StartCoroutine(BlinkObject());
         }
-        else if(isObjectInSocket && !isBlinking)
+        else if (isObjectInSocket && !isBlinking)
         {
             objectToBlink.SetActive(false);
         }
@@ -48,5 +63,15 @@ public class ObjectBlinker : MonoBehaviour
         // 오브젝트가 소켓에 들어가면 깜빡이기 멈춤
         objectToBlink.SetActive(true);
         isBlinking = false;
+    }
+
+    private void OnGrabbed(SelectEnterEventArgs args)
+    {
+        isGrabbed = true;
+    }
+
+    private void OnReleased(SelectExitEventArgs args)
+    {
+        isGrabbed = false;
     }
 }
