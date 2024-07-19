b225ecad4b134b65a2b1f538a237f11ce7114620
Nabyeongho <127078321+nabh0203@users.noreply.github.com>
2024-07-19 17:15:50 +0900

    Soket inventory 구현

--- AnimalSurgicalSimulator_EX/Assets/NBH/Scripts/CustomSoket.cs
+++ AnimalSurgicalSimulator_EX/Assets/NBH/Scripts/CustomSoket.cs
@@ -1,46 +1,18 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class CustomSocketHandler : MonoBehaviour
+public class CustomSocket : XRSocketInteractor
 {
-    public XRSocketInteractor socketInteractor;
-    public Transform[] targetPositions; // 이동할 위치 배열
+    public List<GameObject> allowedObjects = new List<GameObject>();
 
-    private void OnEnable()
+    public override bool CanSelect(IXRSelectInteractable interactable)
     {
-        if (socketInteractor != null)
-        {
-            socketInteractor.selectEntered.AddListener(OnSelectEntered);
-        }
-    }
+        // 기본 조건이 충족되는지 확인
+        if (!base.CanSelect(interactable))
+            return false;
 
-    private void OnDisable()
-    {
-        if (socketInteractor != null)
-        {
-            socketInteractor.selectEntered.RemoveListener(OnSelectEntered);
-        }
-    }
-
-    private void OnSelectEntered(SelectEnterEventArgs args)
-    {
-        // XRGrabInteractable와 ObjectIndex 컴포넌트를 가진 오브젝트만 처리합니다.
-        var interactable = args.interactableObject.transform.GetComponent<XRGrabInteractable>();
-        var objectIndex = args.interactableObject.transform.GetComponent<SocketIndex>();
-
-        if (interactable != null && objectIndex != null)
-        {
-            int index = objectIndex.index;
-
-            // 유효한 인덱스인지 확인합니다.
-            if (index >= 0 && index < targetPositions.Length)
-            {
-                // 오브젝트를 지정한 위치로 이동시킵니다.
-                args.interactableObject.transform.position = targetPositions[index].position;
-                args.interactableObject.transform.rotation = targetPositions[index].rotation;
-            }
-        }
+        // 오브젝트가 허용된 오브젝트 목록에 있는지 확인
+        return allowedObjects.Contains(interactable.transform.gameObject);
     }
 }
