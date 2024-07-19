97bebcf4f076dbd3a4eda5edff57f2ce74bce7f7
poiz1245 <wva960@gmail.com>
2024-07-19 10:36:03 +0900

    코드 정리

--- AnimalSurgicalSimulator_EX/Assets/01.Scripts/DrillTrigger.cs
+++ AnimalSurgicalSimulator_EX/Assets/01.Scripts/DrillTrigger.cs
@@ -56,7 +56,6 @@ public class DrillTrigger : MonoBehaviour
             }
         }
 
-        print(currentTriggerLayerName);
     }
 
     void TriggerButtonOn(InputAction.CallbackContext context)
