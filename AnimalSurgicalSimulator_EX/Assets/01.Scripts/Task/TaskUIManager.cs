3f43f9552c3d34abc79856b4d40b06dd9c751ddc
Nabyeongho <127078321+nabh0203@users.noreply.github.com>
2024-07-22 18:56:47 +0900

    Task UI 제작 중

---
+++ AnimalSurgicalSimulator_EX/Assets/01.Scripts/Task/TaskUIManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskUIManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] taskCompleteUI; // 작업 완료 UI 요소
+
+    private int currentTaskIndex = 0; // 현재 표시할 작업 인덱스
+
+    // 작업 완료 UI 표시
+    public void ShowTaskCompleteUI()
+    {
+        if (currentTaskIndex < taskCompleteUI.Length)
+        {
+            taskCompleteUI[currentTaskIndex].SetActive(true); // 현재 작업 UI 활성화
+            currentTaskIndex++; // 다음 작업으로 이동
+        }
+    }
+
+    public void CloseTaskCompleteUI()
+    {
+        if (currentTaskIndex > 0)
+        {
+            currentTaskIndex--; // 이전 작업으로 이동
+            taskCompleteUI[currentTaskIndex].SetActive(false); // 현재 작업 UI 비활성화
+        }
+    }
+}
