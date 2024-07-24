eb58f9e48919d0089790b7ae3e30f43ee3c33f3a
poiz1245 <wva960@gmail.com>
2024-07-24 12:46:46 +0900

    refactor : task상태 taskManager에서 관리하도록 재조정
    - DigTask에서는 Manager의 상태만 변경
    - Manager에서 상태에 따라서 TextUpdate

--- AnimalSurgicalSimulator_EX/Assets/01.Scripts/TaskManager.cs
+++ AnimalSurgicalSimulator_EX/Assets/01.Scripts/TaskManager.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class TaskManager : MonoBehaviour
 {
     public static TaskManager instance;
 
     public DigComplete digComplete;
+    [SerializeField] DigTask digTask;
+    [SerializeField] TextMeshProUGUI uiText;
+    public enum TaskName
+    {
+        Start,
+        Attach,
+        Dig,
+        Complete
+    }
+
+    public TaskName task = TaskName.Start; // 현재 작업 상태
 
     private void Awake()
     {
@@ -17,6 +30,31 @@ public class TaskManager : MonoBehaviour
         else
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        digTask.OnTaskStateChanged += UpdateUIText;
+        UpdateUIText(task);
+    }
+
+    void UpdateUIText(TaskName taskName)
+    {
+        switch (taskName)
+        {
+            case TaskName.Start:
+                uiText.text = "Grab the drill";
+                break;
+            case TaskName.Attach:
+                uiText.text = "Attach the drill to the guidelines";
+                break;
+            case TaskName.Dig:
+                uiText.text = "Put your fist on the drill";
+                break;
+            case TaskName.Complete:
+                uiText.text = "Bring the drill back to its original position";
+                break;
         }
     }
 }
