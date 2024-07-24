c673120f6adc1e004ca0b025508cce1f577bd759
Nabyeongho <127078321+nabh0203@users.noreply.github.com>
2024-07-24 14:48:22 +0900

    Soket 디테일 수정

--- AnimalSurgicalSimulator_EX/Assets/NBH/Scripts/CustomSoketNBH.cs
+++ AnimalSurgicalSimulator_EX/Assets/NBH/Scripts/CustomSoketNBH.cs
@@ -2,17 +2,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class CustomSocketNBH : XRSocketInteractor
+public class CustomSocketNBH : MonoBehaviour
 {
-    public List<GameObject> allowedObjects = new List<GameObject>();
+    [SerializeField] Transform attach;
+    [SerializeField] LayerMask socketLayer;
+    [SerializeField] GameObject hoverMesh;
+    [SerializeField] List<XRGrabInteractable> selectObjectGrabInteractable;
 
-    public override bool CanSelect(IXRSelectInteractable interactable)
+    [SerializeField] List<CustomSocketNBH> sokets; // 소켓 리스트
+
+    public bool hasSelection = false;
+
+    private void OnTriggerEnter(Collider other)
     {
-        // 기본 조건이 충족되는지 확인
-        if (!base.CanSelect(interactable))
-            return false;
+        if ((socketLayer & (1 << other.gameObject.layer)) != 0)
+        {
+            hoverMesh.SetActive(true);
+        }
+    }
 
-        // 오브젝트가 허용된 오브젝트 목록에 있는지 확인
-        return allowedObjects.Contains(interactable.transform.gameObject);
+    private void OnTriggerStay(Collider other)
+    {
+        for (int i = 0; i < sokets.Count; i++)
+        {
+            // 선택된 오브젝트가 없을 경우
+            if (!selectObjectGrabInteractable[i].isSelected)
+            {
+                hasSelection = true;
+                hoverMesh.SetActive(false);
+                Debug.Log("소켓등록");
+                // 해당 소켓의 위치로 이동
+                selectObjectGrabInteractable[i].transform.position = sokets[i].attach.position;
+                selectObjectGrabInteractable[i].transform.rotation = sokets[i].attach.rotation;
+                
+                return; // 해당 오브젝트가 이동한 후 반복문 종료
+            }
+            else
+            {
+                hoverMesh.SetActive(true);
+                hasSelection = false;
+            }
+        }
+       
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        hoverMesh.SetActive(false);
     }
 }
--- AnimalSurgicalSimulator_EX/Assets/NBH/Scripts/SoketManager.cs
+++ AnimalSurgicalSimulator_EX/Assets/NBH/Scripts/SoketManager.cs
@@ -1,55 +1,55 @@
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.XR.Interaction.Toolkit;
 
-public class SocketManager : MonoBehaviour
-{
-    [System.Serializable]
-    public class SocketConfig
-    {
-        public CustomSocketNBH socket;
-        public List<GameObject> allowedObjects;
-    }
+//public class SocketManager : MonoBehaviour
+//{
+//    [System.Serializable]
+//    public class SocketConfig
+//    {
+//        public CustomSocketNBH socket;
+//        public List<GameObject> allowedObjects;
+//    }
 
-    public SocketConfig[] socketConfigs;
+//    public SocketConfig[] socketConfigs;
 
-    private void OnEnable()
-    {
-        foreach (var config in socketConfigs)
-        {
-            config.socket.allowedObjects = config.allowedObjects;
-            config.socket.selectEntered.AddListener(OnSelectEntered);
-        }
-    }
+//    private void OnEnable()
+//    {
+//        foreach (var config in socketConfigs)
+//        {
+//            config.socket.allowedObjects = config.allowedObjects;
+//            config.socket.selectEntered.AddListener(OnSelectEntered);
+//        }
+//    }
 
-    private void OnDisable()
-    {
-        foreach (var config in socketConfigs)
-        {
-            config.socket.selectEntered.RemoveListener(OnSelectEntered);
-        }
-    }
+//    private void OnDisable()
+//    {
+//        foreach (var config in socketConfigs)
+//        {
+//            config.socket.selectEntered.RemoveListener(OnSelectEntered);
+//        }
+//    }
 
-    private void OnSelectEntered(SelectEnterEventArgs args)
-    {
-        var interactor = args.interactorObject as CustomSocketNBH;
-        var interactableObject = args.interactableObject.transform.gameObject;
+//    private void OnSelectEntered(SelectEnterEventArgs args)
+//    {
+//        var interactor = args.interactorObject as CustomSocketNBH;
+//        var interactableObject = args.interactableObject.transform.gameObject;
 
-        foreach (var config in socketConfigs)
-        {
-            if (config.socket == interactor)
-            {
-                if (config.allowedObjects.Contains(interactableObject))
-                {
-                    // 올바른 오브젝트가 소켓에 들어갔습니다.
-                    return;
-                }
-                else
-                {
-                    // 잘못된 오브젝트가 소켓에 들어갔으므로 제거합니다.
-                    interactor.interactionManager.SelectExit(interactor, args.interactableObject);
-                }
-            }
-        }
-    }
-}
+//        foreach (var config in socketConfigs)
+//        {
+//            if (config.socket == interactor)
+//            {
+//                if (config.allowedObjects.Contains(interactableObject))
+//                {
+//                    // 올바른 오브젝트가 소켓에 들어갔습니다.
+//                    return;
+//                }
+//                else
+//                {
+//                    // 잘못된 오브젝트가 소켓에 들어갔으므로 제거합니다.
+//                    interactor.interactionManager.SelectExit(interactor, args.interactableObject);
+//                }
+//            }
+//        }
+//    }
+//}
