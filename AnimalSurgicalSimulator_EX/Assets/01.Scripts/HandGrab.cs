41487bfbf465ba26cf46626ca5d9d49f40122d58
poiz1245 <wva960@gmail.com>
2024-07-26 18:42:55 +0900

    07.26.18.40
    -메스, 클램프 그랩 가능

--- AnimalSurgicalSimulator_EX/Assets/01.Scripts/HandGrab.cs
+++ AnimalSurgicalSimulator_EX/Assets/01.Scripts/HandGrab.cs
@@ -2,18 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Hands.Gestures;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class HandGrab : MonoBehaviour
 {
     [SerializeField] XRSocketInteractor socketInteractor;
+
+    int currentHandShape;
+
     public void OnGripAction()
     {
         socketInteractor.enabled = true;
     }
 
+    public void LayerSetting()
+    {
+        socketInteractor.interactionLayers = currentHandShape;
+    }
     public void OffGripAction()
     {
         socketInteractor.enabled = false;
+    }
+
+    public void SettingHandShape(int layer)
+    {
+        currentHandShape = layer;
     }
 }
