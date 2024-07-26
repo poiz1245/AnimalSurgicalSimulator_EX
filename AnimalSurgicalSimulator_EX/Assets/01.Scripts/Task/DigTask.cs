724ca7da5eab356b6d6a98aad5c53981f49e1c94
Nabyeongho <127078321+nabh0203@users.noreply.github.com>
2024-07-26 18:52:59 +0900

    UpdateTargets(TaskName taskName) 추가
    1. Task 별로 지정되는 타겟 오브젝트 지정 가능
    할당값
    targets[0] - Drill
    targets[1] - DrillIndicate
    targets[2] - DrillIndicate
    targets[3] - DrillSocketHoverMesh

--- AnimalSurgicalSimulator_EX/Assets/01.Scripts/Task/DigTask.cs
+++ AnimalSurgicalSimulator_EX/Assets/01.Scripts/Task/DigTask.cs
@@ -1,4 +1,94 @@
-using System.Collections;
+//using System.Collections;
+//using System.Collections.Generic;
+//using TMPro;
+//using Unity.VisualScripting;
+//using UnityEngine;
+//using UnityEngine.XR.Interaction.Toolkit;
+//using static TaskManager;
+
+//public class DigTask : MonoBehaviour
+//{
+//    [SerializeField] HandModelControll handModel;
+//    [SerializeField] HandTrackingModelControll hand;
+//    [SerializeField] XRGrabInteractable grab;
+
+//    [SerializeField] TextMeshProUGUI uiText;
+//    [SerializeField] TextMeshProUGUI subUiText;
+
+
+//    public delegate void TaskStateChanged(TaskName task);
+//    public event TaskStateChanged OnTaskStateChanged;
+
+//    private void Start()
+//    {
+//        OnTaskStateChanged += UpdateUIText;
+//        UpdateUIText(TaskManager.instance.task);
+//    }
+
+//    public void Update()
+//    {
+//        switch (TaskManager.instance.task)
+//        {
+//            case TaskName.Start:
+//                if (grab.isSelected)
+//                {
+//                    TaskStateChange(TaskName.Attach);
+//                }
+//                break;
+
+//            case TaskName.Attach:
+//                if (handModel.isAttach || hand.isAttach)
+//                {
+//                    TaskStateChange(TaskName.Dig);
+//                }
+//                else if (!grab.isSelected)
+//                {
+//                    TaskStateChange(TaskName.Start);
+//                }
+//                break;
+
+//            case TaskName.Dig:
+//                if (handModel.currentTaskComplete || hand.currentTaskComplete)
+//                {
+//                    TaskStateChange(TaskName.Complete);
+//                }
+//                else if ((handModel.isAttach == false && hand.isAttach == false))
+//                {
+//                    TaskStateChange(TaskName.Attach);
+//                }
+//                break;
+//        }
+//    }
+
+//    private void TaskStateChange(TaskName taskName)
+//    {
+//        TaskManager.instance.task = taskName;
+//        OnTaskStateChanged?.Invoke(TaskManager.instance.task);
+//    }
+
+//    void UpdateUIText(TaskName taskName)
+//    {
+//        switch (taskName)
+//        {
+//            case TaskName.Start:
+//                uiText.text = "Grab the drill";
+//                subUiText.text = "* Follow the drill guidelines on the right";
+//                break;
+//            case TaskName.Attach:
+//                uiText.text = "Attach the drill to the guidelines";
+//                subUiText.text = "* Hold the object and follow the guidelines";
+//                break;
+//            case TaskName.Dig:
+//                uiText.text = "Put your fist on the drill";
+//                subUiText.text = "* Pull the index finger to activate the drill";
+//                break;
+//            case TaskName.Complete:
+//                uiText.text = "Bring the drill back to its original position";
+//                subUiText.text = "* Take it to the stand. Put your hands down";
+//                break;
+//        }
+//    }
+//}
 using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
@@ -11,9 +101,9 @@ public class DigTask : MonoBehaviour
     [SerializeField] HandModelControll handModel;
     [SerializeField] HandTrackingModelControll hand;
     [SerializeField] XRGrabInteractable grab;
-
     [SerializeField] TextMeshProUGUI uiText;
     [SerializeField] TextMeshProUGUI subUiText;
+    [SerializeField] List<Transform> targets; // 타겟 오브젝트 리스트
 
     public delegate void TaskStateChanged(TaskName task);
     public event TaskStateChanged OnTaskStateChanged;
@@ -22,6 +112,7 @@ public class DigTask : MonoBehaviour
     {
         OnTaskStateChanged += UpdateUIText;
         UpdateUIText(TaskManager.instance.task);
+        OnTaskStateChanged += UpdateTargets; // 타겟 업데이트를 위한 이벤트 추가
     }
 
     public void Update()
@@ -86,5 +177,29 @@ public class DigTask : MonoBehaviour
                 subUiText.text = "* Take it to the stand. Put your hands down";
                 break;
         }
+    }
+
+    void UpdateTargets(TaskName taskName)
+    {
+        List<Transform> newTargets = new List<Transform>();
+
+        // TaskName에 따라 타겟 오브젝트를 변경
+        switch (taskName)
+        {
+            case TaskName.Start:
+                newTargets.Add(targets[0]);
+                break;
+            case TaskName.Attach:
+                newTargets.Add(targets[1]);
+                break;
+            case TaskName.Dig:
+                newTargets.Add(targets[2]);
+                break;
+            case TaskName.Complete:
+                newTargets.Add(targets[3]);
+                break;
+        }
+
+        TaskArrow.Instance.SetTargets(newTargets); // 타겟 업데이트
     }
 }
