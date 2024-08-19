e7e2fa587057eb68b1504071b8a73e057ec80532
poiz1245 <wva960@gmail.com>
2024-08-19 17:33:32 +0900

    08.19
    - UI 뒤로가기 버튼 추가
    - 수술 Scene 가이드 위치 수정

--- AnimalSurgicalSimulator_EX/Assets/01.Scripts/ButtonClick.cs
+++ AnimalSurgicalSimulator_EX/Assets/01.Scripts/ButtonClick.cs
@@ -8,9 +8,11 @@ public class ButtonClick : MonoBehaviour
     [SerializeField] GameObject currentPanel;
     GameObject prevPanel;
 
+    Stack<GameObject> prevPanelList = new Stack<GameObject>();
     public void Focus(GameObject myPanel)
     {
         prevPanel = currentPanel;
+        prevPanelList.Push(prevPanel);
         currentPanel = myPanel;
 
         prevPanel.SetActive(false);
@@ -19,9 +21,14 @@ public class ButtonClick : MonoBehaviour
 
     public void Back()
     {
-        currentPanel = prevPanel;
-        prevPanel = null;
+        prevPanel = currentPanel;
 
+        if (prevPanelList.Count > 0) 
+        {
+            currentPanel = prevPanelList.Pop();
+        }
+
+        prevPanel.SetActive(false);
         currentPanel.SetActive(true);
     }
 
