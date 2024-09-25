485c4351139a7b59c9a983602a51ff6fd93fbc88
poiz1245 <wva960@gmail.com>
2024-09-25 16:13:03 +0900

    09.25

--- AnimalSurgicalSimulator_EX/Assets/01.Scripts/UI/UIPositionReset.cs
+++ AnimalSurgicalSimulator_EX/Assets/01.Scripts/UI/UIPositionReset.cs
@@ -4,37 +4,48 @@ using UnityEngine;
 
 public class UIPositionReset : MonoBehaviour
 {
-    [SerializeField] GameObject dogModel;
-    [SerializeField] GameObject[] crossSections;
+    [SerializeField] Transform dogModel;
+    [SerializeField] Transform[] crossSections;
 
-    Vector3 originalDogPosition;
+    Transform originalDogTransform;
+    Transform[] originalCrossSectionTransform;
+    
+    /*Vector3 originalDogPosition;
     Vector3[] originalCrossSectionPositions;
     Quaternion originalDogRotation;
-    Quaternion[] originalCrossSectionRotations;
+    Quaternion[] originalCrossSectionRotations;*/
 
     void Start()
     {
-        originalDogPosition = dogModel.transform.position;
-        originalDogRotation = dogModel.transform.rotation;
+        originalDogTransform.position = dogModel.position;
+        originalDogTransform.rotation = dogModel.rotation;
 
-        originalCrossSectionPositions = new Vector3[crossSections.Length];
-        originalCrossSectionRotations = new Quaternion[crossSections.Length];
         for (int i = 0; i < crossSections.Length; i++)
         {
-            originalCrossSectionPositions[i] = crossSections[i].transform.position;
-            originalCrossSectionRotations[i] = crossSections[i].transform.rotation;
+            originalCrossSectionTransform[i].position = crossSections[i].transform.position;
+            originalCrossSectionTransform[i].rotation = crossSections[i].transform.rotation;
         }
+        /* originalDogPosition = dogModel.transform.position;
+         originalDogRotation = dogModel.transform.rotation;
+
+         originalCrossSectionPositions = new Vector3[crossSections.Length];
+         originalCrossSectionRotations = new Quaternion[crossSections.Length];
+         for (int i = 0; i < crossSections.Length; i++)
+         {
+             originalCrossSectionPositions[i] = crossSections[i].transform.position;
+             originalCrossSectionRotations[i] = crossSections[i].transform.rotation;
+         }*/
     }
 
     public void ResetPosition()
     {
-        dogModel.transform.position = originalDogPosition;
-        dogModel.transform.rotation = originalDogRotation;
+        dogModel.position = originalDogTransform.position;
+        dogModel.rotation = originalDogTransform.rotation;
 
         for (int i = 0; i < crossSections.Length; i++)
         {
-            crossSections[i].transform.position = originalCrossSectionPositions[i];
-            crossSections[i].transform.rotation = originalCrossSectionRotations[i];
+            crossSections[i].transform.position = originalCrossSectionTransform[i].position;
+            crossSections[i].transform.rotation = originalCrossSectionTransform[i].rotation;
         }
     }
 }
